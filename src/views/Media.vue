10e24af5891049cab9ca42dc48ec8abc4225ec71
City7g <City7gor@gmail.com>
2024-07-16 12:01:55 +0300

    First commit

---
+++ src/views/Media.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h1>Media</h1>
+
+    <div class="hover">
+      <p class="hover__hover">Hover hover</p>
+      <p class="hover__none">Hover none</p>
+    </div>
+
+    <div class="pointer">
+      <p class="pointer__coarse">Pointer coarse</p>
+      <p class="pointer__fine">Pointer fine</p>
+      <p class="pointer__none">Pointer none</p>
+    </div>
+  </div>
+</template>
+
+<style>
+@media (hover: hover) {
+  .hover__hover {
+    color: red;
+  }
+}
+
+@media (hover: none) {
+  .hover__none {
+    color: red;
+  }
+}
+
+@media (pointer: coarse) {
+  .pointer__coarse {
+    color: red;
+  }
+}
+
+@media (pointer: fine) {
+  .pointer__coarse {
+    color: red;
+  }
+}
+
+@media (pointer: none) {
+  .pointer__none {
+    color: red;
+  }
+}
+</style>
